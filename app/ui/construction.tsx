c399553dbd643890166526cb4f0fa6c96dc2cac8
judi-sde <software.engineering.official@gmail.com>
2025-02-23 23:50:49 +0200

    Update construction.tsx

--- app/ui/construction.tsx
+++ app/ui/construction.tsx
@@ -23,7 +23,7 @@ export default function Constructoin() {
     <section className="bg-white dark:bg-gray-900 min-h-screen py-40 lg:py-0">
       <div className="py-8 px-4 mx-auto max-w-screen-xl lg:text-center lg:py-16 lg:flex lg:justify-center lg:items-center">
         <Lottie options={defaultOptions} />
-        <h1 className="m-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Oops! Under Construction</h1>
+        <h1 className="m-4 text-4xl font-extrabold tracking-tight leading-none text-black dark:text-white md:text-5xl lg:text-6xl">Oops! Under Construction</h1>
       </div>
     </section>
 
