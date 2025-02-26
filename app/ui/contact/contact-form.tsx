0a656737a6aa7b4170ae968c37e3b3ea1e90b398
judi-sde <software.engineering.official@gmail.com>
2025-02-27 00:50:45 +0300

    Update contact-form.tsx

--- app/ui/contact/contact-form.tsx
+++ app/ui/contact/contact-form.tsx
@@ -9,7 +9,7 @@ export default function ContactForm({ className }: { className?: string }) {
   
   return (
     <div className={className ? "" : "px-4 -mt-16"}>
-      <form action="#" className={className || "lg:hidden space-y-8 p-4 bg-white border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 dark:bg-gray-800"}>
+      <form action="#" className={className || "lg:hidden space-y-8 p-4 bg-white border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 dark:bg-gray-800 relative z-10"}>
         <div>
           <label htmlFor="contact-info" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">{form["info"].label}</label>
           <input type="text" id="contact-info" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder={form["info"].placeholder} required />
