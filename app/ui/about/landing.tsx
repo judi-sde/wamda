9dd35d1706d24351be7640246814eb39e6721b65
judi-sde <software.engineering.official@gmail.com>
2025-02-27 14:52:19 +0300

    gap

--- app/ui/about/landing.tsx
+++ app/ui/about/landing.tsx
@@ -11,7 +11,7 @@ export default function Landing() {
 
   return (
     <section className="bg-white lg:dark:bg-gray-900 max-lg:h-[350.75px] py-16 lg:text-justify text-center bg-cover dark:bg-gray-700 bg-blend-multiply max-lg:dark:bg-[url(https://flowbite.s3.amazonaws.com/blocks/marketing-ui/contact/laptop-human.jpg)]">
-      <div className="relative z-10 max-w-screen-xl px-4 mx-auto flex justify-center gap-8 mt-[78.75px]">
+      <div className="rtl:gap-56 relative z-10 max-w-screen-xl px-4 mx-auto flex justify-center gap-8 mt-[78.75px]">
         <div>
           <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">{title}</h1>
           <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">{description}</p>
