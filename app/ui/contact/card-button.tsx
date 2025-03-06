e4c18b8b5af78dc6e0a7e2c4a0a6a6c47be5448c
judi-sde <software.engineering.official@gmail.com>
2025-03-06 13:31:15 +0300

    hover effects

--- app/ui/contact/card-button.tsx
+++ app/ui/contact/card-button.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function CardButton({ title, description, href, icon }: { title: string, description: string, href: string, icon: React.ReactNode }) {
 
   return (
-    <Link href={href} className="min-h-[150px] flex-1 p-4 flex flex-col md:flex-row gap-4 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+    <Link href={href} className="min-h-[150px] flex-1 p-4 flex flex-col md:flex-row gap-4 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 max-lg:dark:hover:bg-gray-800  max-lg:dark:hover:border-blue-300">
       <div className="flex justify-center rounded-t-lg md:rounded-none md:rounded-s-lg">
         {icon}
       </div>
